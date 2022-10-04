4847bcee025b4629ea3981c65b1dbbb57b5e8f3f
leonnicolas <leonloechner@gmx.de>
2022-10-04 15:51:00 +0200

    config/config.go: return stat error
    If os.Stat returns an error, we will pass it back instead of always
    returning the os.ErrNotExist.
    
    Signed-off-by: leonnicolas <leonloechner@gmx.de>

--- config/config.go
+++ config/config.go
@@ -182,11 +182,12 @@ func (c *Config) ConfigurePlugins(ctx context.Context, paths []string) (map[stri
 }
 
 func firstPath(paths []string, filename string) (string, error) {
+	err := os.ErrNotExist
 	for _, p := range paths {
 		fpath := filepath.Join(p, filename)
-		if _, err := os.Stat(fpath); err == nil {
+		if _, err = os.Stat(fpath); err == nil {
 			return fpath, nil
 		}
 	}
-	return "", os.ErrNotExist
+	return "", err
 }
