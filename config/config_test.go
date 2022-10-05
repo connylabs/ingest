9849b1d6ed941ab15a507888681473ccea22c9f6
leonnicolas <60091705+leonnicolas@users.noreply.github.com>
2022-10-05 13:57:54 +0200

    cmd/ingest/main_test.go: allow multiple plugin paths (#45)
    * cmd/ingest/main_test.go: allow multiple plugin paths
    
    This PR makes it possible to specify multiple plugin paths. The first
    plugin found will take precedence over plugins specified later.
    
    Signed-off-by: leonnicolas <leonloechner@gmx.de>
    
    * Update config/config.go
    
    Co-authored-by: Lucas Servén Marín <lserven@gmail.com>
    
    * Update cmd/ingest/main.go
    
    Co-authored-by: Lucas Servén Marín <lserven@gmail.com>
    
    * config/config.go: return stat error
    
    If os.Stat returns an error, we will pass it back instead of always
    returning the os.ErrNotExist.
    
    Signed-off-by: leonnicolas <leonloechner@gmx.de>
    
    * config/config.go: early return on non ENOENT errors
    
    When stating a path and we receive an error other than ENOENT (no such
    file or directory) we should exit early and return that unexpected
    error.
    
    Signed-off-by: leonnicolas <leonloechner@gmx.de>
    
    Signed-off-by: leonnicolas <leonloechner@gmx.de>
    Co-authored-by: Lucas Servén Marín <lserven@gmail.com>

---
+++ config/config_test.go
@@ -0,0 +1,84 @@
+package config
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	ctx := context.Background()
+
+	for _, tc := range []struct {
+		name   string
+		paths  []string
+		config []byte
+		err    error
+	}{
+		{
+			name:  "one path",
+			paths: []string{fmt.Sprintf("../bin/plugin/%s/%s", runtime.GOOS, runtime.GOARCH)},
+			config: []byte(`
+sources:
+- name: foo_1
+  type: s3
+- name: foo_2
+  type: s3
+destinations:
+- name: bar_1
+  type: s3
+- name: bar_2
+  type: s3
+workflows:
+- name: foo_1-bar_1
+  source: foo_1
+  destinations:
+  - bar_1
+- name: foo_2-bar_1-bar_2
+  source: foo_2
+  destinations:
+  - bar_1
+  - bar_2
+`),
+		},
+		{
+			name:  "two path",
+			paths: []string{"../bin/plugin/to/nowhere", fmt.Sprintf("../bin/plugin/%s/%s", runtime.GOOS, runtime.GOARCH)},
+			config: []byte(`
+sources:
+- name: foo_1
+  type: s3
+- name: foo_2
+  type: s3
+destinations:
+- name: bar_1
+  type: s3
+- name: bar_2
+  type: s3
+workflows:
+- name: foo_1-bar_1
+  source: foo_1
+  destinations:
+  - bar_1
+- name: foo_2-bar_1-bar_2
+  source: foo_2
+  destinations:
+  - bar_1
+  - bar_2
+`),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			c, err := New(tc.config)
+			require.Nil(t, err)
+
+			_, _, err = c.ConfigurePlugins(ctx, tc.paths)
+			assert.ErrorIs(t, err, tc.err, errors.Unwrap(err))
+		})
+	}
+}
